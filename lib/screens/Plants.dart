8f383282b0b19f27043ceb0976f1cf45ba51cc73
bozhen964 <bozhencomputer964@gmail.com>
2022-06-29 00:01:22 +0300

    bozhen ccreated folders and empty main

--- lib/screens/Plants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
-class Plants extends StatelessWidget {
-  const Plants({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {}
-}
